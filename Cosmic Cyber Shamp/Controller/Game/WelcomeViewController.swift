1955170d700f69172e44d1d11db072b398fb08bd
SwiftCoder <SwiftCoder@nossvwpdf.ls>
2020-07-16 18:01:38 +0300

    add play button

--- Cosmic Cyber Shamp/Controller/Game/WelcomeViewController.swift
+++ Cosmic Cyber Shamp/Controller/Game/WelcomeViewController.swift
@@ -7,6 +7,16 @@ class WelcomeViewController: UIViewController {
         return view
     }()
     
+    let playButton: UIButton = {
+        let button = UIButton()
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 60)
+        button.setTitle("Play", for: .normal)
+        button.setImage(UIImage(named: ""), for: .normal)
+        button.addTarget(self, action: #selector(buttonAction), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadGUI()
@@ -19,10 +29,17 @@ class WelcomeViewController: UIViewController {
         rootView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
         rootView.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
         
+        view.addSubview(playButton)
+        playButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        playButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
     }
     
     override var prefersStatusBarHidden: Bool {
         return true
+    }
+    
+    @objc private func buttonAction() {
+        print("Button pressed")
     }
     
     private func launchTheGame() {
